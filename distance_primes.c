4f70cc8e305d0e10e43dca68ecbfa7dc4d4eb12f
brian <b-sage_88@hotmail.com>
2016-01-27 20:01:18 -0500

    changed while logic and tracking largest diff

--- distance_primes.c
+++ distance_primes.c
@@ -14,12 +14,23 @@ int main(int argc, char **argv) {
   int bound = 1000;
   double largest_diff = 0;
   double temp_diff = 0;
+  MPI_Status  status;
+  
   mpz_t diff;
   mpz_init(diff);
 
   MPI_Init(&argc, &argv); // start mpi
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // process rank
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs); // find out the number of process
+  
+  //setup output file
+  char file_name[15];
+  sprintf(file_name, "rank-%d.txt", my_rank);
+  FILE *f = fopen(file_name, "w");
+  if (f == NULL)
+  {
+      printf("Error opening file!\n");
+  }
 
   int chunk = floor(bound/num_procs);
   int r = fmod(bound, num_procs);
@@ -36,7 +47,7 @@ int main(int argc, char **argv) {
   mpz_init(curr_prime);
 
   //initial
-  printf("My rank: %d \tStarting index: %f\n\n", my_rank, index);
+  fprintf(f, "My rank: %d \t %.0f \t STARTING INDEX\n", my_rank, index);
   mpz_nextprime(prev_prime, m_index);
   mpz_set(curr_prime, prev_prime);
   i = mpz_get_d(prev_prime) + 1;
@@ -48,8 +59,8 @@ int main(int argc, char **argv) {
       largest_diff = temp_diff;
     }
     
-    //printf("My rank: %d \t", my_rank); 
-    //gmp_printf("%Zd \t %Zd\n", curr_prime, diff);
+    fprintf(f, "My rank: %d \t ", my_rank); 
+    gmp_fprintf(f, "%Zd \t %Zd\n", curr_prime, diff);
     
     mpz_set(prev_prime, curr_prime);
     mpz_nextprime(curr_prime, prev_prime);   
@@ -57,10 +68,25 @@ int main(int argc, char **argv) {
   }
 	
   mpz_sub(diff, curr_prime, prev_prime);
-  printf("My rank: %d \t", my_rank);
-  gmp_printf("%Zd \t %Zd \t LAST\n", curr_prime, diff);
+  fprintf(f, "My rank: %d \t ", my_rank);
+  gmp_fprintf(f, "%Zd \t LAST PRIME\n", curr_prime);  
+  fprintf(f, "My rank: %d \t %.0f \t LARGEST DIFF\n", my_rank, largest_diff);
   
-  printf("Largest Difference: %f\n", largest_diff);
+  if (my_rank == 0) {
+    double proc_diff;
+    
+    for (source = 1; source < num_procs; source++) {
+      MPI_Recv(&proc_diff, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+      if (proc_diff > largest_diff) {
+        largest_diff = proc_diff; 
+      }
+    }
+    fprintf(f, "FINAL LARGEST DIFF: %.0f", largest_diff);
+  }
+  else {
+    MPI_Send(&largest_diff, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+  }
 
+  fclose(f);
   MPI_Finalize();
 }
