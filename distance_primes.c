ea0f62cc03fb68251ef8e2e148d126a0942bbaf2
Scott King <kingsc850@gmail.com>
2016-01-29 09:00:35 -0500

    updates

--- distance_primes.c
+++ distance_primes.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
   int num_procs;
   int source;
   int dest;
-  int bound = 1000000;
+  int bound = 1000000000;
   double largest_diff = 0;
   double temp_diff = 0;
   MPI_Status  status;
@@ -20,7 +20,7 @@ int main(int argc, char **argv) {
   MPI_Init(&argc, &argv); // start mpi
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // process rank
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs); // find out the number of process
-  
+
   //calculate chunk size per processor and the remainder
   int chunk = floor(bound/num_procs);
   int r = fmod(bound, num_procs);
@@ -46,48 +46,48 @@ int main(int argc, char **argv) {
   mpz_set(curr_prime, prev_prime);
   i = mpz_get_d(prev_prime) + 1;
 
-  //loop getting primes and calculating differences until the prime is 
+  //loop getting primes and calculating differences until the prime is
   //outside of this processor's chunk size
-  while (i < next_start_index) {  
+  while (i < next_start_index) {
     //subtract current prime from previous
     mpz_sub(diff, curr_prime, prev_prime);
     temp_diff = mpz_get_d(diff);
-    
+
     //if this is the largest difference, store it
     if (temp_diff > largest_diff) {
       largest_diff = temp_diff;
     }
-    
+
     //print data
-    printf("My rank: %d \t ", my_rank); 
+    printf("My rank: %d \t ", my_rank);
     gmp_printf("%Zd \t %Zd\n", curr_prime, diff);
-    
+
     //set the previous prime to the current and get the next prime
     mpz_set(prev_prime, curr_prime);
-    mpz_nextprime(curr_prime, prev_prime);   
+    mpz_nextprime(curr_prime, prev_prime);
     i = mpz_get_d(curr_prime) + 1;
   }
-	
+
   //subtract the previous prime from the current to get difference
   mpz_sub(diff, curr_prime, prev_prime);
-  
+
   //print data
   printf("My rank: %d \t ", my_rank);
-  gmp_printf("%Zd \t LAST PRIME\n", curr_prime);  
+  gmp_printf("%Zd \t LAST PRIME\n", curr_prime);
   printf("My rank: %d \t %.0f \t LARGEST DIFF\n", my_rank, largest_diff);
-  
+
   //if this is proc 0, listen for differences from other procs
   if (my_rank == 0) {
     double proc_diff;
-    
+
     //get difference from each proc and determine largest
     for (source = 1; source < num_procs; source++) {
       MPI_Recv(&proc_diff, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
       if (proc_diff > largest_diff) {
-        largest_diff = proc_diff; 
+        largest_diff = proc_diff;
       }
     }
-    
+
     //print data
     printf("FINAL LARGEST DIFF: %.0f", largest_diff);
   }
